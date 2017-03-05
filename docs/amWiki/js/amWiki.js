56ff9ce153d15dfd93dc8642f8142519702c51f3
YaoXuanZhi <yaoxuanzhi@outlook.com>
2017-03-05 12:03:50 +0800

    - 为此文档添加Debugger for Chrome的调试配置信息 - 屏蔽异常“sourceRequest error:illegal handle”

--- docs/amWiki/js/amWiki.js
+++ docs/amWiki/js/amWiki.js
@@ -283,7 +283,8 @@ $(function () {
         $main.children('.main-inner').scrollTop(0);  //返回顶部
         //更新history记录
         if (!withOutPushState && HISTORY_STATE) {
-            history.pushState({path: path}, '', '?file=' + path);
+            //注释掉此行，因为这里抛出了异常“sourceRequest error:illegal handle”
+            // history.pushState({path: path}, '', '?file=' + path);
         }
         //第二步，加载服务器上的文档资源，如果有更新重新渲染
         docs.loadPage(path, function (state, content) {
